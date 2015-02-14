b147d1beffab6446c672a8ec36247ef05b3631f0
Rich-Harris <richard.a.harris@gmail.com>
2015-02-14 11:40:45 -0500

    add bundling example

--- gobblefile.js
+++ gobblefile.js
@@ -8,11 +8,21 @@ module.exports = gobble([
 	// include bower_components
 	gobble( 'bower_components' ),
 
-	// transpile src/js file with 6to5, creating
-	// AMD modules (NOT CommonJS) so that they
-	// can be loaded with RequireJS
-	gobble( 'src/js' ).transform( '6to5', {
-		modules: 'amd'
-	})
+	gobble( 'src/js' )
+
+		// transpile src/js file with 6to5, creating
+		// AMD modules (NOT CommonJS) so that they
+		// can be loaded with RequireJS
+		.transform( '6to5', {
+			modules: 'amd'
+		})
+
+		// then use the RequireJS optimizer to
+		// bundle the modules into a single file
+		.transform( 'requirejs', {
+			name: 'app',
+			out: 'app.js',
+			name: 'app'
+		})
 
 ]);
